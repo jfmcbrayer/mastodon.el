29d68f65f7c38b779338b4977cb48c2853fb86e4
Johnson Denen <johnson.denen@gmail.com>
2017-04-29 22:12:54 -0400

    Add cleanup to unit test

--- test/mastodon-timelime-tests.el
+++ test/mastodon-timelime-tests.el
@@ -2,12 +2,13 @@
 
 (ert-deftest mastodon-timeline--define-get ()
   "Should define a function that displays timeline data."
-  (with-temp-buffer
-    (with-mock
-      (mock (mastodon-http--api "timeline/foobar") => "this-is-a-url-string")
-      (mock (mastodon-http--get-json "this-is-a-url-string") => "json-content")
-      (mock (switch-to-buffer "*mastodon-foobar*"))
-      (mock (mastodon-timeline--display "json-content"))
-      (mock (mastodon-mode))
-      (mastodon-timeline--define-get "timeline/foobar" "foobar")
-      (mastodon-foobar--get-timeline))))
+  (unwind-protect
+      (with-mock
+        (mock (mastodon-http--api "timeline/foobar") => "this-is-a-url-string")
+        (mock (mastodon-http--get-json "this-is-a-url-string") => "json-content")
+        (mock (switch-to-buffer "*mastodon-foobar*"))
+        (mock (mastodon-timeline--display "json-content"))
+        (mock (mastodon-mode))
+        (mastodon-timeline--define-get "timeline/foobar" "foobar")
+        (mastodon-foobar--get-timeline))
+    (kill-buffer "*mastodon-foobar*")))

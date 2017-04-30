a17720c1093a3c640d9712d6d7580abcd8e71a0b
Johnson Denen <johnson.denen@gmail.com>
2017-04-29 22:08:31 -0400

    Add mastodon-timeline feature

---
+++ lisp/mastodon-timeline.el
@@ -0,0 +1,54 @@
+;;; mastodon-timeline.el --- Timeline behavior functions and macros
+
+;; Copyright (C) 2017 Johnson Denen
+;; Author: Johnson Denen <johnson.denen@gmail.com>
+;; Version: 0.6.2
+;; Homepage: https://github.com/jdenen/mastodon.el
+
+;; This file is not part of GNU Emacs.
+
+;; This file is part of mastodon.el.
+
+;; mastodon.el is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; mastodon.el is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with mastodon.el.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; mastodon-timeline.el is a library of functions and macros to
+;; help provide timeline-like behavior.
+
+;;; Code:
+
+(defgroup mastodon-timeline nil
+  "Timeline-like behavior for Mastodon."
+  :prefix "mastodon-timeline-"
+  :group  'mastodon)
+
+(defmacro mastodon-timeline--define-get (endpoint name)
+  "Define an interactive timeline function to retrieve ENDPOINT data.
+
+Use NAME in definition."
+  (let* ((func (intern (format "mastodon-%s--get-timeline" name)))
+         (docs (format "Open %s timeline." name)))
+    `(defun ,func () ,docs
+            (interactive)
+            (let* ((url (mastodon-http--api ,endpoint))
+                   (buffer (format "*mastodon-%s*" ,name))
+                   (json (mastodon-http--get-json url)))
+              (with-output-to-temp-buffer buffer
+                (switch-to-buffer buffer)
+                (mastodon-timeline--display json))
+              (mastodon-mode)))))
+
+(provide 'mastodon-tiemline)
+;;; mastodon-tl.el ends here
---
+++ test/mastodon-timelime-tests.el
@@ -0,0 +1,13 @@
+(require 'el-mock)
+
+(ert-deftest mastodon-timeline--define-get ()
+  "Should define a function that displays timeline data."
+  (with-temp-buffer
+    (with-mock
+      (mock (mastodon-http--api "timeline/foobar") => "this-is-a-url-string")
+      (mock (mastodon-http--get-json "this-is-a-url-string") => "json-content")
+      (mock (switch-to-buffer "*mastodon-foobar*"))
+      (mock (mastodon-timeline--display "json-content"))
+      (mock (mastodon-mode))
+      (mastodon-timeline--define-get "timeline/foobar" "foobar")
+      (mastodon-foobar--get-timeline))))
